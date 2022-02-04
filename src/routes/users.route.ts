111424386750ebc17f7edc5d58068833328be6de
Jean-Carlos-A-Souza <jean.carlos.alb.souza@gmail.com>
2022-02-04 15:55:09 -0300

    first commit

---
+++ src/routes/users.route.ts
@@ -0,0 +1,37 @@
+import { Router, Request, Response, NextFunction } from "express";
+import statusCodes from 'http-status-codes';
+
+const  usersRoute = Router();
+
+usersRoute.get('/users', (req: Request, res: Response, next: NextFunction)=> {
+    const users =[{userName: 'Jean Carlos'}];
+    res.status(statusCodes.OK).send(users);
+});
+
+usersRoute.get('/users/:uuid', (req: Request, res: Response, next: NextFunction)=> {
+   const uuid = req.params.uuid;
+   
+    res.status(statusCodes.OK).send({uuid});
+});
+
+usersRoute.post('/users',(req: Request, res: Response, next: NextFunction)=> {
+    const newUser = req.body;
+    res.status(statusCodes.CREATED).send(newUser);
+
+});
+
+usersRoute.put('/users/:uuid', (req: Request, res: Response, next: NextFunction)=> {
+    const uuid = req.params.uuid;
+    const modifiedUser = req.body;
+
+    modifiedUser.uuid = uuid;
+
+    res.status(statusCodes.OK).send(modifiedUser)
+});
+
+usersRoute.delete('/users/:uuid', (req: Request, res: Response, next: NextFunction)=> {
+    res.sendStatus(statusCodes.OK);
+});
+
+
+export default usersRoute;
